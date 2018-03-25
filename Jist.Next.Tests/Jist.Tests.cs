dc12fbedf27ec0aa349acd1d9e0f519e61586b85
Tyler Watson <tyler@tw.id.au>
2018-03-25 11:36:04 +1000

    Next wip

--- Jist.Next.Tests/Jist.Tests.cs
+++ Jist.Next.Tests/Jist.Tests.cs
@@ -9,8 +9,36 @@ public class JistTests
     public void LoadTypeScriptShouldLoadAndExecuteTheTypeScriptEngine()
     {
         var jist = new Jist.Next.Jist(Path.GetTempPath());
-        jist.LoadTypeScript();
 
         Assert.NotEqual(JsValue.Undefined, jist.Engine.Global.Get("ts"));
+    }
+
+    [Fact(DisplayName = "It should transpile a module in typescript")]
+    public void ItShouldTranspileAModule()
+    {
+        Directory.SetCurrentDirectory(Path.GetTempPath());
+
+        File.WriteAllText("index-ts.ts", @"
+            export const str: string = ""test"";
+        ");
+
+        var jist = new Jist.Next.Jist(Path.GetTempPath());
+        var exports = jist.ModuleLoadingEngine.RunMain("./index-ts");
+
+        Assert.Equal("test", exports.AsObject().Get("str").AsString());
+    }
+
+    [Fact(DisplayName = "It should throw an error when a module can't be compiled because of a TypeScript error")]
+    public void ItShouldThrowAnErrorWhenATypeScriptDiagnosticMessageHappens()
+    {
+        Directory.SetCurrentDirectory(Path.GetTempPath());
+
+        File.WriteAllText("error-ts.ts", @"
+            export function blah
+        ");
+
+        var jist = new Jist.Next.Jist(Path.GetTempPath());
+
+        Assert.Throws<Exception>(() => jist.ModuleLoadingEngine.RunMain("./error-ts"));
     }
 }
--- Jist.Next/Jist.cs
+++ Jist.Next/Jist.cs
@@ -1,7 +1,9 @@
+using System;
 using System.IO;
 using System.IO.Compression;
 using Jint;
 using Jint.CommonJS;
+using Jint.Native;
 
 namespace Jist.Next
 {
@@ -11,7 +13,7 @@ namespace Jist.Next
         {
         });
 
-        internal ModuleLoadingEngine ModuleLoadingEngine { get; set; }
+        public ModuleLoadingEngine ModuleLoadingEngine { get; internal set; }
 
         public string ScriptsDirectory { get; internal set; }
 
@@ -22,6 +24,40 @@ namespace Jist.Next
 
             SetupScriptsDirectory();
             LoadTypeScript();
+        }
+
+        private JsValue TranspileModule(string path, IModule module)
+        {
+            var ts = Engine.Global.Get("ts").AsObject();
+            var transpileModule = ts.Get("transpileModule");
+            var asm = typeof(Jist).Assembly;
+            var sourceCode = File.ReadAllText(path);
+
+            var transpileConfig = new {
+                compilerOptions = new {
+                    target = "es5",
+                    strict = true,
+                },
+                fileName = Path.GetFileName(path),
+            };
+
+            var compileObject = transpileModule.Invoke(new JsValue[] { sourceCode, JsValue.FromObject(Engine, transpileConfig) }).AsObject();
+            /*
+                    { outputText: '"use strict";\nexports.__esModule = true;\nexports.str = "test";\n',
+                    diagnostics: [],
+                    sourceMapText: undefined }
+             */
+            var diagnostics = compileObject.Get("diagnostics").AsArray();
+
+            if (diagnostics.GetLength() > 0)
+            {
+                throw new Exception();
+            }
+
+            var exports = (module as Module).Compile(compileObject.Get("outputText").AsString(), path).AsObject();
+            module.Exports = exports;
+
+            return exports;
         }
 
         public void SetupScriptsDirectory()
@@ -50,6 +86,10 @@ namespace Jist.Next
             {
                 Engine.Execute(sr.ReadToEnd());
             }
+
+            ModuleLoadingEngine.FileExtensionParsers[".ts"] = TranspileModule;
+            ModuleLoadingEngine.FileExtensionParsers[".js"] = TranspileModule;
+            ModuleLoadingEngine.FileExtensionParsers["default"] = TranspileModule;
         }
     }
 }
